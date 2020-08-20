ff03964164730c7bd82d9feef4cd43fddbcbd71a
Jesse <nguyenjessev@gmail.com>
2020-08-20 19:32:51 -0400

    Add Node class

--- bst.rb
+++ bst.rb
@@ -1,4 +1,19 @@
 # frozen-string-literal: true
 
 module BinarySearchTree
+  # This class represents a node in the tree
+  class Node
+    include Comparable
+    attr_accessor :data, :left, :right
+
+    def <=>(other)
+      data <=> other.data
+    end
+
+    def initialize(data, left, right)
+      @data = data
+      @left = left
+      @right = right
+    end
+  end
 end
