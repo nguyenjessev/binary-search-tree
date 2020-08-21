deff2be47bcd6962ec694182212bcd4df04c5d08
Jesse <nguyenjessev@gmail.com>
2020-08-20 21:58:45 -0400

    Add build_tree and pretty_print methods

--- bst.rb
+++ bst.rb
@@ -1,5 +1,7 @@
 # frozen-string-literal: true
 
+require 'pry'
+
 module BinarySearchTree
   # This class represents a node in the tree
   class Node
@@ -21,8 +23,34 @@ module BinarySearchTree
   class Tree
     attr_accessor :root
 
-    def initialize(array); end
+    def initialize(array)
+      @root = build_tree(array)
+    end
 
-    def build_tree(array); end
+    def build_tree(array)
+      return nil if array.empty?
+
+      nodes = array.uniq.sort
+      middle = (nodes.length - 1) / 2
+      left_nodes = middle > 0 ? nodes[0..(middle - 1)] : []
+      right_nodes = nodes[(middle + 1)..-1]
+
+      middle_node = Node.new(nodes[middle], build_tree(left_nodes), build_tree(right_nodes))
+
+      middle_node
+    end
+
+    def pretty_print(node = @root, prefix = '', is_left = true)
+      pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+      puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+      pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    end
   end
 end
+
+test = Array.new(15) { rand(1..100) }
+test_tree = BinarySearchTree::Tree.new(test)
+puts
+p test
+puts
+test_tree.pretty_print
