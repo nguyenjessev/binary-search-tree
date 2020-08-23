656d1c3219b7a813e7e1f9f6ee012ec4055a661a
Jesse <nguyenjessev@gmail.com>
2020-08-23 00:20:38 -0400

    Add driver code

--- bst.rb
+++ bst.rb
@@ -177,3 +177,21 @@ module BinarySearchTree
     end
   end
 end
+
+test = Array.new(15) { rand(1..100) }
+tree = BinarySearchTree::Tree.new(test)
+puts "Balanced?: #{tree.balanced?}"
+puts "Level order: #{tree.level_order.join(', ')}"
+puts "Preorder: #{tree.preorder.join(', ')}"
+puts "Postorder: #{tree.postorder.join(', ')}"
+puts "Inorder: #{tree.inorder.join(', ')}"
+10.times do
+  tree.insert(rand(100..200))
+end
+puts "Balanced?: #{tree.balanced?}"
+tree.rebalance
+puts "Balanced?: #{tree.balanced?}"
+puts "Level order: #{tree.level_order.join(', ')}"
+puts "Preorder: #{tree.preorder.join(', ')}"
+puts "Postorder: #{tree.postorder.join(', ')}"
+puts "Inorder: #{tree.inorder.join(', ')}"
